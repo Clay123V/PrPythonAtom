dfc94abc98263040dcba4e4ae7adb6b4a3f9f4fb
Kirill Kormashov <kirill-kormashov@yandex.ru>
2018-10-23 21:12:55 +0300

    Добавил writer

---
+++ homeworks/02/my_python_functions/fib_functions/my_functions.py
@@ -0,0 +1,16 @@
+def simple_decorator(function):
+    def check_func(arg):
+        if arg <= -1 : 
+            raise Exception
+        else : 
+            return function(arg)
+    return check_func
+
+    
+@simple_decorator
+def fib(n):
+    if n == 0 : 
+        return 0
+    if n < 3 :
+        return 1
+    return fib(n-1) + fib(n-2)
